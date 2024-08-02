ee2dfeaa488e96fd8b6fa0a723ef4ffd18477827
Prakhar Saxena <29610365+sxnaprkhr@users.noreply.github.com>
2024-08-02 21:27:22 +0530

    feature: navbar + avatar + toggle theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import AuthProvider from "@/context/authProvider";
+import { Navbar } from "@/components/nav";
 import "./globals.css";
+import { ThemeProvider } from "@/context/themeProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +20,17 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <AuthProvider>{children}</AuthProvider>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <AuthProvider>
+            <Navbar />
+            {children}
+          </AuthProvider>
+        </ThemeProvider>
       </body>
     </html>
   );
