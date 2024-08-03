4ae942ed9caa5d6037c5f60111de9c1030521c2b
Prakhar <29610365+sxnaprkhr@users.noreply.github.com>
2024-08-03 11:30:22 +0530

    Feature/navbar (#3)
    * feature: navbar + avatar + toggle theme
    
    * add: config based navbar

--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -5,6 +5,8 @@ import { HamburgerMenuIcon } from "@radix-ui/react-icons";
 import { useWindowDimensions } from "@/hooks/useWindowDimensions";
 import NavAvatar from "./NavAvatar";
 import NavToggle from "./NavToggle";
+// import navConfig from "@/lib/navData";
+import { NavDataType, NavGroupInterface } from "./nav.types";
 
 interface Composition {
   children: ReactNode;
@@ -63,28 +65,45 @@ const NavItem = (props: Composition) => {
   const { children } = props;
 
   return (
-    <div className="text-white sm:mr-3 p-2 border rounded font-semibold border-none">
+    <div className="background-nav text-white sm:mr-3 p-2 border rounded font-semibold border-none">
       {children}
     </div>
   );
 };
 
 export const Navbar = () => {
+  const [navConfig, setNavConfig] = useState<NavDataType>([]);
+
+  useEffect(() => {
+    const fetchNavbarConfig = async () => {
+      const response = await fetch("/api/config");
+      const { data } = await response.json();
+      setNavConfig(data.navData as NavDataType);
+    };
+
+    fetchNavbarConfig();
+  }, []);
+
+  const navItemsMap = {
+    logo: NavLogo,
+    item: NavItem,
+    avatar: NavAvatar,
+    toggle: NavToggle,
+  };
+
   return (
     <NavContainer>
       <NavRenderer>
-        <NavGroup>
-          <NavLogo>LOGO</NavLogo>
-        </NavGroup>
-        <NavGroup>
-          <NavItem>Your Work</NavItem>
-          <NavItem>Projects</NavItem>
-          <NavItem>Filters</NavItem>
-        </NavGroup>
-        <NavGroup>
-          <NavAvatar />
-          <NavToggle />
-        </NavGroup>
+        {navConfig?.map((navGroup) => (
+          <NavGroup key={navGroup?.id}>
+            {navGroup?.items?.map((navItem) => {
+              const Component = navItemsMap[navItem?.type] || <></>;
+              return (
+                <Component key={navItem?.id}>{navItem?.content}</Component>
+              );
+            })}
+          </NavGroup>
+        ))}
       </NavRenderer>
     </NavContainer>
   );
---
+++ src/lib/navData.ts
@@ -0,0 +1,47 @@
+const navConfig = [
+  {
+    id: 0,
+    items: [
+      {
+        id: 0,
+        type: "logo",
+        content: "LOGO",
+      },
+    ],
+  },
+  {
+    id: 1,
+    items: [
+      {
+        id: 0,
+        type: "item",
+        content: "Your Work",
+      },
+      {
+        id: 1,
+        type: "item",
+        content: "Projects",
+      },
+      {
+        id: 2,
+        type: "item",
+        content: "Filters",
+      },
+    ],
+  },
+  {
+    id: 2,
+    items: [
+      {
+        id: 0,
+        type: "avatar",
+      },
+      {
+        id: 1,
+        type: "toggle",
+      },
+    ],
+  },
+];
+
+export default navConfig;
